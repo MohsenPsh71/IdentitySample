4f8cb5d327b2d1b9cefd1e8568f5b1492c9d58bb
mohsen <71mohsenpsh@gmail.com>
2025-05-03 20:04:59 +0330

    Add anti-forgery-secured validation for email and username
    Added `IsEmailInUse` and `IsUserNameInUse` `HttpPost` methods
    in `AccountController` to validate email and username
    availability with anti-forgery protection. Updated
    `RegisterViewModel` to use these methods with `[Remote]`
    validation attributes, ensuring secure client-side validation.

--- IdentitySample/Controllers/AccountController.cs
+++ IdentitySample/Controllers/AccountController.cs
@@ -116,6 +116,8 @@ namespace IdentitySample.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> IsEmailInUse(string email)
         { 
            var user =  await _userManager.FindByEmailAsync(email);
@@ -123,6 +125,8 @@ namespace IdentitySample.Controllers
            return Json("ایمیل وارد شده از قبل موجود است");
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> IsUserNameInUse(string userName)
         {
             var user = await _userManager.FindByNameAsync(userName);

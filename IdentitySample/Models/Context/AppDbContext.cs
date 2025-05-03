fda4316af221bcbad4ff68947a1fc74173785bd1
mohsen <71mohsenpsh@gmail.com>
2025-05-03 12:08:10 +0330

    Add project files.

---
+++ IdentitySample/Models/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentitySample.Models.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
---
+++ IdentitySample/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySample.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? City { get; set; }
+        public string? Gender { get; set; }
+    }
+}
